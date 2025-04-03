00828d3a0c7f188642e0c7f25e40ab924483ad6e
Aleksandar <aleksandar.petos@cern.ch>
2025-04-03 14:52:21 +0200

    Added files from my-cmssw/ported-producer

---
+++ DataFormats/L1TrackTrigger/src/L1TkMuonParticle.cc
@@ -0,0 +1,74 @@
+// -*- C++ -*-
+//
+// Package:     L1Trigger
+// Class  :     L1TkMuonParticle
+
+#include "DataFormats/L1TrackTrigger/interface/L1TkMuonParticle.h"
+
+using namespace l1t;
+
+
+L1TkMuonParticle::L1TkMuonParticle( const LorentzVector& p4,
+         const edm::Ref< RegionalMuonCandBxCollection > &muRef,
+         const edm::Ptr< L1TTTrackType >& trkPtr,
+         float tkisol )
+   : L1Candidate( p4 ),
+     muRef_ ( muRef ) ,
+     trkPtr_ ( trkPtr ) ,
+     theIsolation ( tkisol ),
+     TrkzVtx_(999),
+     quality_(999)
+{
+
+ if ( trkPtr_.isNonnull() ) {
+	float z = getTrkPtr() -> POCA().z();
+	setTrkzVtx( z );
+ }
+}
+
+L1TkMuonParticle::L1TkMuonParticle( const LorentzVector& p4,
+         const edm::Ref< EMTFHitCollection > &muStubRef,
+         const edm::Ptr< L1TTTrackType >& trkPtr,
+         float tkisol )
+   : L1Candidate( p4 ),
+     muStubRef_ ( muStubRef ) ,
+     trkPtr_ ( trkPtr ) ,
+     theIsolation ( tkisol ),
+     TrkzVtx_(999),
+     quality_(999)
+{
+
+ if ( trkPtr_.isNonnull() ) {
+	float z = getTrkPtr() -> POCA().z();
+	setTrkzVtx( z );
+ }
+}
+
+
+
+L1TkMuonParticle::L1TkMuonParticle( const LorentzVector& p4,
+         const edm::Ptr< L1TTTrackType >& trkPtr,
+         float tkisol )
+   : L1Candidate( p4 ),
+     trkPtr_ ( trkPtr ) ,
+     theIsolation ( tkisol ),
+     TrkzVtx_(999),
+     quality_(999)
+{
+
+ if ( trkPtr_.isNonnull() ) {
+	float z = getTrkPtr() -> POCA().z();
+	setTrkzVtx( z );
+ }
+}
+
+
+
+void L1TkMuonParticle::addBarrelStub(const L1MuKBMTCombinedStubRef& stub) {
+  barrelStubs_.push_back(stub);
+}
+
+void L1TkMuonParticle::addMuonStub(const edm::Ref< EMTFHitCollection > &muStubRef) {
+  muStubRefs_.push_back(muStubRef);
+}
+
